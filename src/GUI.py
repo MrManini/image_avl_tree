4003b7a01fa2bd83b3396ad0f766c90b8d1e3793
Pipe Mancini <pipemancini03@gmail.com>
2024-03-17 22:26:02 -0500

    changes to GUI

--- src/GUI.py
+++ src/GUI.py
@@ -1,6 +1,27 @@
 import tkinter as tk
+import graphviz as gv
+from avl import Tree, ABB, AVL
+from Node import Node
+    
 
+# Clase UI para crear la interfaz gráfica de TKinter
 class UI:
+    node = Node(1)
+    node2 = Node(2)
+    node3 = Node(3)
+    node4 = Node(4)
+    node5 = Node(5)
+
+    avl = AVL(node)
     def __init__(self):
         root = tk.Tk()
+
+        root.geometry("600x500")
+        root.title("Laboratorio Arbol AVL")
+
+        label = tk.Label(root, text = "Representación Arbol AVL", anchor = "center", font = ("Courier", 20))
+        label.pack(pady = 15)
+
         root.mainloop()
+    
+    

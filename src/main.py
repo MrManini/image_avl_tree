74ad004047c51043c3c1ed356440282eb31ed5b6
Pipe Mancini <pipemancini03@gmail.com>
2024-03-17 23:16:38 -0500

    remove error

--- src/main.py
+++ src/main.py
@@ -6,15 +6,6 @@ from GUI import UI
 def main() -> None:
     ui = UI()
 
-    avl = AVL()
-    avl.insert("1")
-    avl.insert("2")
-    avl.insert("3")
-    avl.insert("4")
-    avl.insert("5")
-
-    avl.plot()
-
 
 if __name__ == '__main__':
     main()

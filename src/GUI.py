bd47e5b2178401594fe8f55d99ab30ccf8d391a2
Pipe Mancini <pipemancini03@gmail.com>
2024-03-20 15:26:00 -0500

    update GUI

--- src/GUI.py
+++ src/GUI.py
@@ -11,6 +11,8 @@ class UI:
     def __init__(self, graph: "AVL"):
         root = tk.Tk()
 
+        self.graph = graph
+
         root.geometry("600x500")
         root.title("Laboratorio Arbol AVL")
 
@@ -20,6 +22,22 @@ class UI:
         pngGraph = tk.PhotoImage(file = PATH)
         labelGraph = tk.Label(image = pngGraph)
         labelGraph.pack(pady = 15)
+        
+        def InsertNodes(graph: "AVL", label, pngGraph):
+            graph.add_some_nodes()
+            label.config(image = pngGraph)
+            graph.plot()
+        
+        def DeleteNodes(graph: "AVL", label, pngGraph):
+            graph.del_som_nodes()
+            label.config(image = pngGraph)
+            graph.plot()
+
+        butonInsert = tk.Button(InsertNodes(graph, labelGraph, pngGraph))
+        butonInsert.pack(pady = 15)
+
+        butonDelete = tk.Button(DeleteNodes(graph, labelGraph, pngGraph))
+        butonDelete.pack(pady = 15)
 
         root.mainloop()
     
--- src/main.py
+++ src/main.py
@@ -5,12 +5,6 @@ from GUI import UI
 
 def main() -> None:
     avl = AVL()
-    
-    avl.insert("bike_001.bmp")
-    avl.insert("bike_002.bmp")
-    avl.insert("bike_004.bmp")
-
-    avl.plot()
     gui = UI(avl)
 
 
